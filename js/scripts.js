2d4cc2955b753b264df9a25ba4f34abf61d62674
Sophie Hughes <sophie.hughes07@gmail.com>
2020-02-11 12:48:18 +0000

    Added inital hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("hello");
+
+function burgerToggle() {
+  var x = document.getElementById("navBar");
+  if (x.className === "navBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
