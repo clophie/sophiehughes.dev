d968efa2da00cf39aa019941d4b90c524d88fd43
Sophie Hughes <sophie.hughes07@gmail.com>
2020-03-10 19:20:07 +0000

    Added visibility toggle to about button

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
-console.log("hello");
+"use strict";
+
+const aboutButton = document.querySelector("#aboutButton");
+const projectsButton = document.querySelector("#projectsButton");
+const experienceButton = document.querySelector("#experienceButton");
+const photoButton = document.querySelector("#photoButton");
+const name = document.querySelector("#name");
+const aboutText = document.querySelector("#aboutText");
+const navBar = document.querySelector("#navBar");
 
 function burgerToggle() {
-  var x = document.getElementById("navBar");
-  if (x.className === "navBar") {
-    x.className += " responsive";
+  if (navBar.className === "navBar") {
+    navBar.className += " responsive";
   } else {
-    x.className = "navBar";
+    navBar.className = "navBar";
   }
 }
+
+var clickEvent = (() => {
+  if ('ontouchstart' in document.documentElement)
+    return 'touchstart';
+  else
+    return 'click';
+})();
+
+aboutButton.addEventListener(clickEvent, () => {
+  if (name.style.display === "none") {
+    name.style.display = "inline-block";
+    aboutText.style.display = "none";
+  } else {
+    name.style.display = "none";
+    aboutText.style.display = "inline-block";
+  }
+});
