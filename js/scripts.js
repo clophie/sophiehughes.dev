4ca52fec8dbcd2ddbd92bf1cf01df3d20c1194e0
Sophie Hughes <sophie.hughes07@gmail.com>
2020-03-11 20:07:55 +0000

    Processed data from api and populated a table with it

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ const photoButton = document.querySelector("#photoButton");
 const name = document.querySelector("#name");
 const aboutText = document.querySelector("#aboutText");
 const navBar = document.querySelector("#navBar");
+let table = document.querySelector("#animeTable");
 let animeJson = '';
 
 function burgerToggle() {
@@ -17,7 +18,7 @@ function burgerToggle() {
   }
 }
 
-var clickEvent = (() => {
+let clickEvent = (() => {
   if ('ontouchstart' in document.documentElement)
     return 'touchstart';
   else
@@ -28,9 +29,11 @@ aboutButton.addEventListener(clickEvent, () => {
   if (name.style.display === "none") {
     name.style.display = "inline-block";
     aboutText.style.display = "none";
+    table.style.display = "none;"
   } else {
     name.style.display = "none";
     aboutText.style.display = "inline-block";
+    table.style.display = "inline-block";
   }
 });
 
@@ -38,6 +41,30 @@ aboutButton.addEventListener(clickEvent, () => {
 fetch('https://api.jikan.moe/v3/user/clophie/animelist/watching')
   .then(response => response.json())
   .then(data => {
-    animeJson = data;
     console.log(data);
-  })
+    populateTable(data);});
+
+const populateTable = (data) => {
+
+  // helper function
+  function addCell(tr, text) {
+    let td = tr.insertCell();
+    td.textContent = text;
+    return td;
+  }
+
+  // create header
+  let tableHead = table.createTHead();
+  let headerRow = tableHead.insertRow();
+  addCell(headerRow, '');
+  addCell(headerRow, 'Title');
+  addCell(headerRow, 'Episodes Watched');
+
+  // insert data
+  data.anime.forEach((item) => {
+    let row = table.insertRow();
+    addCell(row, item.image_url);
+    addCell(row, item.title);
+    addCell(row, item.watched_episodes + '/' + item.total_episodes);
+  });
+};
